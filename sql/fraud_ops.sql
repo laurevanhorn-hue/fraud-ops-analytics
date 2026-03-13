7630361d391d98f08efdf58c81651cf44cc31278
laurevanhorn-hue <laurevanhorn@gmail.com>
2026-03-13 18:55:33 -0500

    Update fraud_ops.sql

--- sql/fraud_ops.sql
+++ sql/fraud_ops.sql
@@ -1,1 +1,28 @@
+-- Fraud Operations Analytics
+-- SQL examples based on the Kaggle Credit Card Fraud dataset
+-- Columns used:
+-- Time   = seconds since first transaction
+-- Amount = transaction amount
+-- Class  = fraud flag (1 = fraud, 0 = non-fraud)
 
+-- 1. Fraud rate by transaction amount bucket
+
+WITH amount_buckets AS (
+    SELECT
+        CASE
+            WHEN Amount < 10 THEN 'under_10'
+            WHEN Amount < 50 THEN '10_to_49'
+            WHEN Amount < 100 THEN '50_to_99'
+            ELSE '100_plus'
+        END AS amount_bucket,
+        Class
+    FROM creditcard
+)
+SELECT
+    amount_bucket,
+    COUNT(*) AS total_transactions,
+    SUM(Class) AS fraud_transactions,
+    ROUND(AVG(Class) * 100, 3) AS fraud_rate_pct
+FROM amount_buckets
+GROUP BY amount_bucket
+ORDER BY fraud_rate_pct DESC;
